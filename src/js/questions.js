e8616fb30416124052aabbf61ed708bb5ff860bd
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2023-02-01 12:58:58 +0100

    feat: create quiz questions and answers array

---
+++ src/js/questions.js
@@ -0,0 +1,51 @@
+const questions = [
+  {
+    id: 1,
+    question: "Co to jest DBMS?",
+    answer_1: "Obiektowy język programowania do generowania baz danych",
+    answer_2: "System zarządzania bazą danych",
+    answer_3: "rozszerzenie pliku konfiguracyjnego bazdy danych",
+    answer_4: "Strukturalny język zapytań kierowanych do bazy danych",
+    correct_answer: 2,
+  },
+  {
+    id: 2,
+    question:
+      "Deklaracja w języku JavaScript: let x = 'true'; powoduje, że zmienna x jest typu",
+    answer_1: "logicznego",
+    answer_2: "tekstowego",
+    answer_3: "wyliczeniowego",
+    answer_4: "liczbowego",
+    correct_answer: 2,
+  },
+  {
+    id: 3,
+    question: "Który z kodów szesnastkowych oznacza kolor żółty?",
+    answer_1: "#FFFF00",
+    answer_2: "#FF00FF",
+    answer_3: "#00FFFF",
+    answer_4: "#F0F0F0",
+    correct_answer: 1,
+  },
+  {
+    id: 4,
+    question: "Który z wymienionych systemów nie jest systemem CMS?",
+    answer_1: "Drupal",
+    answer_4: "WordPress",
+    answer_3: "Joomla!",
+    answer_2: "Figma",
+    correct_answer: 4,
+  },
+  {
+    id: 5,
+    question:
+      "Aby zamieścić aplikację webową w internecie, należy jej pliki źródłowe skopiować na serwer za pomocą protokołu",
+    answer_1: "HTTP",
+    answer_2: "HTTPS",
+    answer_3: "FTP",
+    answer_4: "AJAX",
+    correct_answer: 3,
+  },
+];
+
+export default questions;
