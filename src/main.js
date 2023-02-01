0b465c092a3d89035cbd2eb0006337283b1ca876
Filip Statkiewicz <filip.statkiewicz25@gmail.com>
2023-02-01 19:04:39 +0100

    feat: logic of checking answers

--- src/main.js
+++ src/main.js
@@ -2,35 +2,53 @@ import questions from "./js/questions.js";
 class Quiz {
   constructor() {
     this.question_counter = 0;
+    this.score = 0;
+    this.value;
 
     this.submit_button = document.querySelector(".submit-button");
     this.question = document.querySelector(".game-board__question-content");
     this.answer = [...document.querySelectorAll(".game-board__answer")];
 
-    this.render_question();
-    this.submit_button.addEventListener("click", this.nextQuestion.bind(this));
-
-    // console.log(this.question.innerText);
-    // console.log(this.answer[0].innerText);
-    // console.log(this.answer[1].innerText);
-    // console.log(this.answer[2].innerText);
-    // console.log(this.answer[3].innerText);
-    // console.log(questions[0].correct_answer);
+    this.renderQuestion();
+    this.submit_button.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.nextQuestion();
+    });
   }
 
-  nextQuestion(e) {
-    e.preventDefault();
-    this.question_counter++;
-    // console.log("zostałem kliknięty");
-    // console.log(this.question_counter);
-    this.render_question();
-  }
-
-  render_question() {
-    this.question.innerText = questions[this.question_counter].question;
-    for (let i = 0; i < this.answer.length; i++) {
-      this.answer[i].innerText = questions[this.question_counter].answer[i];
+  checkAnswer() {
+    if (this.value === questions[this.question_counter].correct_answer) {
+      this.score++;
     }
+  }
+
+  nextQuestion() {
+    this.checkAnswer();
+    this.question_counter++;
+    this.renderQuestion();
+  }
+
+  renderQuestion() {
+    this.question.innerHTML = questions[this.question_counter].question;
+    for (let i = 0; i < this.answer.length; i++) {
+      this.answer[i].innerHTML = `<input type="radio" name="answer${
+        this.question_counter
+      }" id="${i}" value="${i}">
+        ${(this.answer[i].innerText =
+          questions[this.question_counter].answer[i])}`;
+    }
+
+    this.answer = [...document.querySelectorAll(".game-board__answer")];
+    this.input = [
+      ...document.getElementsByName(`answer${this.question_counter}`),
+    ];
+
+    this.input.forEach((input) => {
+      input.addEventListener("click", (e) => {
+        this.value = e.target.value;
+        this.value = parseInt(this.value);
+      });
+    });
   }
 }
 
